87e7b6713fdd7fed6068b55ba2b4af34f0f5cc5e
ArshaanSoftware <mullaharshaan926@gmail.com>
2026-04-27 21:55:13 +0530

    Update Typescript script

---
+++ Index.ts
@@ -0,0 +1,22 @@
+let num : number = 585
+console.log(num)
+
+let str : string = "Arshaan"
+console.log(str)
+
+let isActive : boolean = true
+console.log(isActive)
+
+// Method 1 : Using square brackets 
+
+let numbers : number[] = [1 , 2 , 3 ,4 ,5 , 6]
+console.log(numbers)
+
+let names : string[] = ["Arshaan" , "CEO" , "SoftwareDeveloper"]
+console.log(names)
+
+let booleans : boolean[] = [true , false , true , false , true]
+console.log(booleans)
+
+let flt : number[] = [ 1 , 2 , 3 , 4]
+console.log(flt)
